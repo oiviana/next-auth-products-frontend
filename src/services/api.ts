8ebb6074f57a70aedcd6240e882f0560f01e469e
decobot <capy@deco.cx>
2025-10-21 17:06:51 -0300

    refactor: test base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-});
+  baseURL: `https://${process.env.NEXT_PUBLIC_BACKEND_URL}`,
 
 api.interceptors.request.use((config) => {
   const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
