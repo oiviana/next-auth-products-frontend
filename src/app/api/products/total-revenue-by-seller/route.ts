dcfc40705cc8e0c4495e1e459fe8a779f1e4b18d
oiviana <vianalucas80@gmail.com>
2025-10-22 00:07:47 -0300

    refactor: using next router handlers

---
+++ src/app/api/products/total-revenue-by-seller/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL; 
+  try {
+
+    const token = req.headers.get("authorization");
+
+
+    const res = await fetch(`${backendUrl}/products/total-revenue-by-seller`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token ? { Authorization: token } : {}),
+      },
+      cache: "no-store", 
+    });
+    
+    const data = await res.json();
+
+    return NextResponse.json(data, { status: res.status });
+  } catch (error) {
+    console.error("Erro ao buscar produtos:", error);
+    return NextResponse.json(
+      { error: "Erro interno no servidor" },
+      { status: 500 }
+    );
+  }
+}
